3ae8eaa7b374ff9bd7c89db29b2613b567f64228
itsprashuk <91676231+itsprashuk@users.noreply.github.com>
2023-07-03 17:59:58 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+import * as faceapi from 'face-api.js';
 
 function App() {
+  const videoheight = 480;
+  const videowidth = 640;
+  const [initializing, setInitializing] = useState(false);
+  const videoref = useRef();
+  const canvaref = useRef();
+
+  useEffect(() => {
+    const loadModels = async () => {
+      const MODEL_URL = process.env.PUBLIC_URL + './models';
+      setInitializing(true);
+      Promise.all([
+        faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+        faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+        faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+        faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL),
+      ]).then(startVideo);
+    };
+    loadModels();
+  }, []);
+
+  const startVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: {} })
+      .then(stream => {
+        videoref.current.srcObject = stream;
+      })
+      .catch(error => {
+        console.error('Error accessing camera', error);
+      });
+  };
+
+  const handlevideo=()=>{
+    setInterval(async()=>{
+      if(initializing){
+        setInitializing(false);
+      }
+      canvaref.current.innerHTML=faceapi.createCanvasFromMedia(videoref.current);
+      const displaysize={
+        width:videowidth,
+        height:videoheight
+      }
+      faceapi.matchDimensions(canvaref.current,displaysize);
+      const detection=await faceapi.detectAllFaces(videoref.current,new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+
+      const resizedetection=faceapi.resizeResults(detection,displaysize);
+      canvaref.current.getContext('2d').clearRect(0,0,videowidth,videoheight);
+
+      faceapi.draw.drawDetections(canvaref.current,resizedetection);
+      faceapi.draw.drawFaceLandmarks(canvaref.current,resizedetection);
+      faceapi.draw.drawFaceExpressions(canvaref.current,resizedetection);
+      
+
+      
+
+    },1000)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span>{initializing ? 'Initializing' : 'Ready'}</span>
+
+      <div className='display-flex justify-content-center'>
+ <video ref={videoref} autoPlay muted height={videoheight} width={videowidth} onPlay={handlevideo} />
+      <canvas ref={canvaref} className='position-absolute'></canvas>
+      </div>
+     
     </div>
   );
 }
